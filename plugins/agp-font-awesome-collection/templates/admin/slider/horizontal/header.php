c0bc16e13f3597623816974205d11cf2d5b2b52e
rodge025 <iea.domondon@gmail.com>
2019-10-22 11:02:27 +0800

    Add files via upload

---
+++ plugins/agp-font-awesome-collection/templates/admin/slider/horizontal/header.php
@@ -0,0 +1,9 @@
+<?php
+?>
+    <th>Headline</th>
+    <th>Description</th>
+    <th>Icon</th>    
+    <th>Link URL</th>
+    <th>Text Color</th>
+    <th>Background Color</th>    
+    <th class="tbl-actions">Actions</th>
